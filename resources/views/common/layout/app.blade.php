6176be99bbe42caa0fb8f2ceafe1e685befd903f
AhMing <keeshengming@outlook.com>
2022-05-03 16:05:27 +0800

    chore: extend app layout

---
+++ resources/views/common/layout/app.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>@yield('title')</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @yield('style')
+</head>
+
+<body>
+@yield('menubar')
+@yield('header')
+<main style="margin-top: 60px;">@yield('content')</main>
+@yield('footer')
+<script src="{{ asset('js/manifest.js') }}"></script>
+<script src="{{ asset('js/vendor.js') }}"></script>
+<script src="{{ asset('js/app.js') }}"></script>
+@yield('script')
+</body>
+
+</html>
---
+++ resources/views/listing/ch/layout/app.blade.php
@@ -0,0 +1,13 @@
+@extends('common.layout.app')
+
+@section('menubar')
+    @include('listing.ch.layout.menubar')
+@endsection
+
+@section('header')
+    @include('listing.ch.layout.header')
+@endsection
+
+@section('footer')
+    @include('listing.ch.layout.footer')
+@endsection
