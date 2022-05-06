80b532f87fe688a1568fa9ad1b62a898253d8980
AhMing <keeshengming@outlook.com>
2022-05-07 05:58:04 +0800

    chore: add system config table and shipping configs

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -38,6 +38,46 @@ class DatabaseSeeder extends Seeder
         $this->order();
         $this->customer();
         $this->orderItem();
+        $this->systemConfig();
+    }
+
+    private function systemConfig()
+    {
+        DB::table('system_configs')
+            ->insert(
+                [
+                    'name' => 'shipping_fee',
+                    'value' => '3',
+                    'desc' => '运输费用',
+                ]
+            );
+
+        DB::table('system_configs')
+            ->insert(
+                [
+                    'name' => 'shipping_discount_is_activated',
+                    'value' => '1',
+                    'desc' => '订单免运费开关',
+                ]
+            );
+
+        DB::table('system_configs')
+            ->insert(
+                [
+                    'name' => 'shipping_discount_threshold',
+                    'value' => '50',
+                    'desc' => '订单免运费触发价格',
+                ]
+            );
+
+        DB::table('system_configs')
+            ->insert(
+                [
+                    'name' => 'shipping_discount_desc',
+                    'value' => '满RM50包邮',
+                    'desc' => '订单免运费详情',
+                ]
+            );
     }
 
     private function orderItem()
