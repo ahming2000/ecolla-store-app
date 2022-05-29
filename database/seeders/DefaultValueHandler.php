34894b94a5dd2c1b68ff1532e8eb87450b098f40
ahming2000 <keeshengming@outlook.com>
2022-05-29 15:35:55 +0800

    style: rearrange reused code

---
+++ database/seeders/DefaultValueHandler.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\PaymentMethod;
+use App\Models\SystemConfig;
+
+class DefaultValueHandler {
+    public static function import()
+    {
+        self::paymentMethod();
+        self::systemConfig();
+    }
+
+    private static function paymentMethod() {
+        $names = [
+            'Touch \'n Go',
+            'Boost Pay',
+            'Online Banking',
+            'Maybank QR Pay',
+            'Quin Pay'
+        ];
+
+        $icons = [
+            '/images/tng.png',
+            '/images/boost.png',
+            '/images/online-banking.png',
+            '/images/maybank-qr-pay.png',
+            '/images/quin-pay.png',
+        ];
+
+        $qrCode = [
+            '/images/tng-qr-code.jpeg',
+            '/images/boost-qr-code.jpeg',
+            '/images/online-banking-qr-code.jpeg',
+            '/images/maybank-qr-pay-qr-code.jpeg',
+            '/images/quin-pay-qr-code.jpeg',
+        ];
+
+        for ($i = 0; $i < sizeof($names); $i++) {
+            $paymentMethod = new PaymentMethod(
+                [
+                    'name' => $names[$i],
+                    'icon' => $icons[$i],
+                    'qr_code' => $qrCode[$i],
+                ]
+            );
+            $paymentMethod->save();
+        }
+    }
+
+    private static function systemConfig() {
+        $list[] = new SystemConfig(
+            [
+                'name' => 'shipping_fee',
+                'value' => '3',
+                'desc' => '运输费用',
+            ]
+        );
+
+        $list[] = new SystemConfig(
+            [
+                'name' => 'shipping_discount_is_activated',
+                'value' => '1',
+                'desc' => '订单免运费开关',
+            ]
+        );
+
+        $list[] = new SystemConfig(
+            [
+                'name' => 'shipping_discount_threshold',
+                'value' => '50',
+                'desc' => '订单免运费触发价格',
+            ]
+        );
+
+        $list[] = new SystemConfig(
+            [
+                'name' => 'shipping_discount_desc',
+                'value' => '满RM50包邮',
+                'desc' => '订单免运费详情',
+            ]
+        );
+
+        foreach($list as $config) {
+            $config->save();
+        }
+    }
+}
--- database/seeders/DevSeeder.php
+++ database/seeders/DevSeeder.php
@@ -9,8 +9,6 @@ use App\Models\ItemImage;
 use App\Models\Order;
 use App\Models\OrderItem;
 use App\Models\Origin;
-use App\Models\PaymentMethod;
-use App\Models\SystemConfig;
 use App\Models\User;
 use App\Models\Variation;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -27,8 +25,9 @@ class DevSeeder extends Seeder
      */
     public function run()
     {
+        DefaultValueHandler::import();
+
         $this->category();
-        $this->paymentMethod();
         $this->origin();
         $this->user();
         $this->item();
@@ -37,46 +36,6 @@ class DevSeeder extends Seeder
         $this->order();
         $this->customer();
         $this->orderItem();
-        $this->systemConfig();
-    }
-
-    private function systemConfig()
-    {
-        $list[] = new SystemConfig(
-                [
-                    'name' => 'shipping_fee',
-                    'value' => '3',
-                    'desc' => '运输费用',
-                ]
-            );
-
-        $list[] = new SystemConfig(
-                [
-                    'name' => 'shipping_discount_is_activated',
-                    'value' => '1',
-                    'desc' => '订单免运费开关',
-                ]
-            );
-
-        $list[] = new SystemConfig(
-                [
-                    'name' => 'shipping_discount_threshold',
-                    'value' => '50',
-                    'desc' => '订单免运费触发价格',
-                ]
-            );
-
-        $list[] = new SystemConfig(
-                [
-                    'name' => 'shipping_discount_desc',
-                    'value' => '满RM50包邮',
-                    'desc' => '订单免运费详情',
-                ]
-            );
-
-        foreach($list as $config) {
-            $config->save();
-        }
     }
 
     private function orderItem()
@@ -198,44 +157,6 @@ class DevSeeder extends Seeder
                 ]
             );
             $category->save();
-        }
-    }
-
-    private function paymentMethod()
-    {
-        $names = [
-            'Touch \'n Go',
-            'Boost Pay',
-            'Online Banking',
-            'Maybank QR Pay',
-            'Quin Pay'
-        ];
-
-        $icons = [
-            '/images/tng.png',
-            '/images/boost.png',
-            '/images/online-banking.png',
-            '/images/maybank-qr-pay.png',
-            '/images/quin-pay.png',
-        ];
-
-        $qrCode = [
-            '/images/tng-qr-code.jpeg',
-            '/images/boost-qr-code.jpeg',
-            '/images/online-banking-qr-code.jpeg',
-            '/images/maybank-qr-pay-qr-code.jpeg',
-            '/images/quin-pay-qr-code.jpeg',
-        ];
-
-        for ($i = 0; $i < sizeof($names); $i++) {
-            $paymentMethod = new PaymentMethod(
-                [
-                    'name' => $names[$i],
-                    'icon' => $icons[$i],
-                    'qr_code' => $qrCode[$i],
-                ]
-            );
-            $paymentMethod->save();
         }
     }
 }
