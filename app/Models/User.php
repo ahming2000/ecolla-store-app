338486b596a02d3df91ca592d3d930243410b507
ahming2000 <keeshengming@outlook.com>
2022-05-30 12:02:51 +0800

    chore: add soft delete to the user model and delete some tables

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,11 +5,12 @@ namespace App\Models;
 use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Model implements Authenticatable
 {
-    use HasApiTokens, HasFactory, \Illuminate\Auth\Authenticatable, ReturnRegionDateTime;
+    use HasApiTokens, HasFactory, \Illuminate\Auth\Authenticatable, ReturnRegionDateTime, SoftDeletes;
 
     protected $fillable = [
         'username',
