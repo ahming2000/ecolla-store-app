eeb61a0e1ad188aa714bd891f777ee1a04dc441c
AhMing <keeshengming@outlook.com>
2022-06-13 13:51:59 +0800

    style: add order mode display to check out page

--- app/Enum/OrderMode.php
+++ app/Enum/OrderMode.php
@@ -6,4 +6,19 @@ class OrderMode
 {
     public static int $SELF_PICKUP = 0;
     public static int $DELIVERY = 1;
+
+    public static function getLabel($orderMode, $lang): string
+    {
+        if ($lang == 'en') {
+            return match ($orderMode) {
+                self::$DELIVERY => 'Delivery',
+                default => 'Pick-up',
+            };
+        } else {
+            return match ($orderMode) {
+                self::$DELIVERY => '外送',
+                default => '预购取货',
+            };
+        }
+    }
 }
