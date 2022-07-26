c5e395eea992a1be7d81541cf8b25992bcb93e55
AhMing <keeshengming@outlook.com>
2022-07-26 14:00:50 +0800

    feat: add paginate automation

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,15 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected function estimatePagination(int $totalCount, int $minItem = 40, int $maxPage = 9, int $itemPerRow = 4): int
+    {
+        if ($totalCount <= $minItem) return $minItem;
+
+        while (floor($totalCount / $minItem) >= $maxPage) {
+            $minItem = $minItem + $itemPerRow;
+        }
+
+        return $minItem;
+    }
 }
