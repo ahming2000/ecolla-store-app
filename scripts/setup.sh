6a8fec83c7ad2cb5f02aa684998b2802252d4e60
AhMing <keeshengming@outlook.com>
2022-10-10 16:19:37 +0800

    Edit typo in setup.sh and add prod seeding option in package.json

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+echo WARNING: THIS SETUP SHOULD ONLY RUN ON DEVELOPMENT
+read -n 1 -s -r -p "Press any key to continue..."
+
 echo Installing packages...
 composer install
 npm install
+
+echo Compiling webpack...
+npm run dev
 
 echo Copying .env.example to .env...
 cp .env.example .env
@@ -14,8 +20,6 @@ echo Generating symbolic links for files uploading...
 php artisan storage:link
 
 echo Migrating database tables and generating faker data...
-npm run db:fresh
+npm run db:dev
 
 echo Done.
-
-
