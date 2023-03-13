bb45f121b627ae5cad2e2784370fa786f763053f
ahming2000 <keeshengming@outlook.com>
2023-03-13 23:01:39 +0800

    Add route name to all routes

--- routes/listing.php
+++ routes/listing.php
@@ -6,19 +6,19 @@ use App\Http\Controllers\ItemController;
 use App\Http\Controllers\SettingController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/lang/{lang}', [SettingController::class, 'changeLanguage']);
+Route::get('/lang/{lang}', [SettingController::class, 'changeLanguage'])->name('changeLanguage');
 
-Route::get('/', [InfoController::class, 'landingPage']);
-Route::get('/payment-method', [InfoController::class, 'paymentMethodPage']);
+Route::get('/', [InfoController::class, 'landingPage'])->name('landingPage');
+Route::get('/payment-method', [InfoController::class, 'paymentMethodPage'])->name('paymentMethodPage');
 
 Route::prefix('/item')->group(function () {
-   Route::get('/', [ItemController::class, 'listingPage']);
-   Route::get('/{item}', [ItemController::class, 'singleListingPage']);
+   Route::get('/', [ItemController::class, 'listingPage'])->name('listingPage');
+   Route::get('/{item}', [ItemController::class, 'singleListingPage'])->name('singleListingPage');
 });
 
 Route::prefix('cart')->group(function () {
-    Route::get('/', [CartController::class, 'cartPage']);
-    Route::get('/check-out', [CartController::class, 'checkOutPage']);
+    Route::get('/', [CartController::class, 'cartPage'])->name('cartPage');
+    Route::get('/check-out', [CartController::class, 'checkOutPage'])->name('checkOutPage');
 
-    Route::post('/check-out', [CartController::class, 'checkOut']);
+    Route::post('/check-out', [CartController::class, 'checkOut'])->name('checkOut');
 });
